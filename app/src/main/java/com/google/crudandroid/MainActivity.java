9d530640df7cda6c86d7fee658503159e5a5677a
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-11-27 18:41:31 +0700

    sampai toast response volley create

--- app/src/main/java/com/google/crudandroid/MainActivity.java
+++ app/src/main/java/com/google/crudandroid/MainActivity.java
@@ -3,6 +3,20 @@ package com.google.crudandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +24,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText editText = findViewById(R.id.value);
+        Button button = findViewById(R.id.submit);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String data = editText.getText().toString();
+                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                String url ="http://192.168.1.26/crud-android/create.php";
+
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                if (response.equals("Success")){
+                                    Toast.makeText(MainActivity.this, "Data added", Toast.LENGTH_SHORT).show();
+                                }
+                                else
+                                    Toast.makeText(MainActivity.this, response, Toast.LENGTH_LONG).show();
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                }){
+                    protected Map<String, String> getParams(){
+                        Map<String, String> paramV = new HashMap<>();
+                        paramV.put("id", "1");
+                        paramV.put("data", data);
+                        return paramV;
+                    }
+                };
+                queue.add(stringRequest);
+            }
+        });
     }
 }
